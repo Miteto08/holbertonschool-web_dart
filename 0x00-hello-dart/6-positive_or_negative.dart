8776dc8771c0ff7d9d3e7ea6135fa2bd2453248c
Miteto08 <julien.lechevalier@shortways.com>
2025-06-06 14:09:34 +0200

    task 6. Positive anything is better than negative nothing

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,10 @@
+void main(args) {
+  int nb = int.parse(args[0]);
+  if (nb > 0) {
+    print("${nb} is positive");
+  } else if (nb < 0) {
+    print("${nb} is negative");
+  } else {
+    print("${nb} is zero");
+  }
+}
