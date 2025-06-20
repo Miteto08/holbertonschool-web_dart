c5c7f0c04e987d598683c1e36a9a69500a4729ed
Miteto08 <julien.lechevalier@shortways.com>
2025-06-20 11:11:43 +0200

    Task 6. Inheritance

--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -8,9 +8,12 @@ class Password {
   bool isValid() {
     return _password.length >= 8 && 
            _password.length <= 16 && 
-           _password.contains(RegExp(r'[0-9]')) &&
-           _password.contains(RegExp(r'[a-z]')) &&
-           _password.contains(RegExp(r'[A-Z]'));
+           _password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
   }
 
   String get password => _password;
