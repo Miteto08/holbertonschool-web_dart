e2935d76097cbe357a5a08d4b24c20297e3cf64f
Miteto08 <julien.lechevalier@shortways.com>
2025-06-20 10:38:24 +0200

    Task 4. Mutables

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,23 @@
+class Password {
+  String _password = "";
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  bool isValid() {
+    return _password.length >= 8 && 
+           _password.length <= 16 && 
+           _password.contains(RegExp(r'[0-9]'));
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+
+  String get password => _password;
+  set password(String password) {
+    _password = password;
+  }
+}
