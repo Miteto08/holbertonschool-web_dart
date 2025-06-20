7cc3f333976ef4fe738b141e41a9cde0649674c0
Miteto08 <julien.lechevalier@shortways.com>
2025-06-20 11:05:40 +0200

    Task 6. Inheritance

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,39 @@
+import '6-password.dart';
+
+class User extends Password {
+  final String? name;
+  final int? age;
+  final double? height;
+  final int? id;
+
+  User({required this.name, required this.age, required this.height, required this.id, required String user_password}) 
+      : super(password: user_password);
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id ?? 0,
+      'name': name ?? "",
+      'age': age ?? 0,
+      'height': height ?? 0.0,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'] ?? 0,
+      name: userJson['name'] ?? "",
+      age: userJson['age'] ?? 0,
+      height: userJson['height'] ?? 0.0,
+      user_password: userJson['user_password'] ?? "",
+    );
+  }
+
+  @override
+  String toString() {
+    return "User(id : ${id ?? 0} ,name: ${name ?? ""}, age: ${age ?? 0}, height: ${height ?? 0.0}, Password: ${isValid()})";
+  }
+
+  set user_password(String newPassword) {
+    password = newPassword;
+  }
+}
---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,19 @@
+class Password {
+  String? _password = "";
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  bool isValid() {
+    return _password != null && 
+           _password!.length >= 8 && 
+           _password!.length <= 16 && 
+           _password!.contains(RegExp(r'[0-9]'));
+  }
+
+  String? get password => _password;
+  set password(String? password) {
+    _password = password;
+  }
+}
