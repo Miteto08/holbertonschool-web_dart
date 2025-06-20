ef8607efcba5057ceedb9d21702736567a7c1b6d
Miteto08 <julien.lechevalier@shortways.com>
2025-06-20 10:45:56 +0200

    Task 5. Json to User

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,31 @@
+class User {
+  final String name;
+  final int age;
+  final double height;
+  final int id;
+
+  User({required this.name, required this.age, required this.height, required this.id});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+    );
+  }
+
+  @override
+  String toString() {
+    return "User(id : $id ,name: $name,  age: $age, height: $height)";
+  }
+}
